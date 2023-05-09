529290c4ec8c2aec6d1721e3511314f6613176a2
udomk <kufrekus4@gmail.com>
2023-05-09 15:14:28 +0100

    Add project files.

---
+++ StructExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mark m = new Mark(6, 7);
+            m.DoThis();
+        }
+    }
+    public struct Mark
+    {
+        public int x;
+        public int y;
+
+        public Mark(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+
+        public void DoThis()
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+}
